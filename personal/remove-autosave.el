89c5516f38676d28745830a9ed77e3327112b52c
Craig Vyvial <cp16net@gmail.com>
2015-02-19 12:28:50 -0600

    moving the autosave and backup files to /tmp

---
+++ personal/remove-autosave.el
@@ -0,0 +1,7 @@
+;;
+;; store the backup and autosave in the system's tmp directory
+;;
+(setq backup-directory-alist
+      `((".*" . ,temporary-file-directory)))
+(setq auto-save-file-name-transforms
+      `((".*" ,temporary-file-directory t)))
